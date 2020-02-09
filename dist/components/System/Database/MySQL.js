46076c1d8f623c133bee9924f27895fff4132539
Mudassar Islam <demoncious@gmail.com>
2020-02-09 22:21:07 +0500

    True

--- dist/components/System/Database/MySQL.js
+++ dist/components/System/Database/MySQL.js
@@ -5,16 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const module_1 = require("./../../../module");
 const sync_mysql_1 = __importDefault(require("sync-mysql"));
-const knex_1 = __importDefault(require("knex"));
 class MySQL {
     constructor(details) {
         this._log = new module_1.Logger();
         this._conn = null;
         this.builder = null;
         this._conn = new sync_mysql_1.default(details);
-        this.builder = knex_1.default({
-            client: 'mysql'
-        });
     }
     query(SQL_Query, bindings = null) {
         return this._conn.query(SQL_Query, bindings);
